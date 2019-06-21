97262f2aef62cac05de6824160391d799868b233
Oliver Pietsch <oliver.pietsch@publicispixelpark.de>
2019-06-21 20:51:57 +0200

    Adds text component

---
+++ src/components/TextComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p v-for="(text, index) in content" :key="index">
+      <span v-html="text"></span>
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "TextComponent"
+})
+export default class TextComponent extends Vue {
+  @Prop() content!: Array<string>;
+}
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <TextComponent :content="content"></TextComponent>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import TextComponent from "@/components/TextComponent.vue"; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld
+    TextComponent
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  content: Array<string> = [
+    "Hallo, ich bin Oliver.",
+    'Ich gestalte und entwickle Websites mit <a href="https://vuejs.org/" title="Vue.js" target="_blank">Vue.js</a>',
+    'Zur Zeit arbeite ich bei <a href="https://www.publicispixelpark.de/" title="Publicis Pixelpark" target="_blank">Publicis Pixelpark</a>.',
+    'Du kannst mich <a href="mailto:kontakt@oliver-pietsch.de">hier</a> (<a href="https://oliver-pietsch.de/kontakt_at_oliver-pietsch.de_public_key.asc" target="_blank">PGP Key</a>) kontaktieren.'
+  ];
+}
 </script>
