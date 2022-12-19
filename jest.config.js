6ac659171e9c9ef6b71ebb9829a3ce4a0eef2361
Oliver Pietsch <kontakt@oliver-pietsch.de>
2022-12-19 17:44:45 +0100

    #210 Fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   moduleFileExtensions: ["js", "jsx", "json", "vue", "ts", "tsx"],
   transform: {
-    "^.+\\.vue$": "vue3-jest",
+    "^.+\\.vue$": "@vue/vue3-jest",
     ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
       "jest-transform-stub",
     "^.+\\.tsx?$": "ts-jest",
@@ -15,14 +15,12 @@ module.exports = {
   testMatch: [
     "**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)",
   ],
-  testURL: "http://localhost/",
+  testEnvironmentOptions: {
+    url: "http://localhost/",
+    customExportConditions: ["node", "node-addons"],
+  },
   watchPlugins: [
     "jest-watch-typeahead/filename",
     "jest-watch-typeahead/testname",
   ],
-  globals: {
-    "ts-jest": {
-      babelConfig: true,
-    },
-  },
 };
